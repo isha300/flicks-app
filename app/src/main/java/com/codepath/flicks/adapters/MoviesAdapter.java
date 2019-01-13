2e9d47569a0b2c04c232150c5ffbc6ffa2da4912
Isha Kabra <isha.kabra10@gmail.com>
2019-01-13 01:17:26 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/flicks/adapters/MoviesAdapter.java
@@ -0,0 +1,77 @@
+package com.codepath.flicks.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.flicks.R;
+import com.codepath.flicks.models.Movie;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MoviesAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Log.d("smile", "onCreateViewHolder");
+        View view = LayoutInflater.from(context).inflate(R.layout.item_movie, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("smile", "onCreateViewHolder: " + position);
+        Movie movie = movies.get(position);
+        // Bind the movie data into the view holder
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageURL = movie.getPosterPath();
+            // Reference the backdrop path if phone is in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageURL = movie.getBackdropPath();
+            }
+            Glide.with(context).load(imageURL).into(ivPoster);
+        }
+    }
+
+}
