068eab8ec4cf430e27d429489bb70675fe32c09e
Isha Kabra <isha.kabra10@gmail.com>
2019-01-25 19:09:53 -0500

    Created detailed info page for each movie

--- app/src/main/java/com/codepath/flicks/adapters/MoviesAdapter.java
+++ app/src/main/java/com/codepath/flicks/adapters/MoviesAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.flicks.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.Image;
 import android.support.annotation.NonNull;
@@ -10,11 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.codepath.flicks.DetailActivity;
 import com.codepath.flicks.R;
 import com.codepath.flicks.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,16 +59,19 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
 
         TextView tvTitle;
         TextView tvOverview;
+        TextView popularity;
         ImageView ivPoster;
+        RelativeLayout container;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageURL = movie.getPosterPath();
@@ -71,6 +80,16 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
                 imageURL = movie.getBackdropPath();
             }
             Glide.with(context).load(imageURL).into(ivPoster);
+            // Add click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Navigate to detail activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 
--- app/src/main/java/com/codepath/flicks/models/Movie.java
+++ app/src/main/java/com/codepath/flicks/models/Movie.java
@@ -3,22 +3,34 @@ package com.codepath.flicks.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    double popularity;
+    int movieId;
+    double voteAverage;
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+
+    // empty constructor needed by the Parceler library
+    public Movie() {
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         backdropPath = jsonObject.getString("backdrop_path");
+        voteAverage = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+        popularity = jsonObject.getDouble("popularity");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -43,5 +55,17 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public double getPopularity() {
+        return popularity;
     }
 }
